6fa68a970f3a9983e2d89d1701a2ef72e01497af
Skye <anastasia_sorokina_90@mail.ru>
2024-03-18 13:59:57 +0300

    pages styling added

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,6 @@
 # Pin npm packages by running ./bin/importmap
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
+pin "semantic-ui", to: "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
