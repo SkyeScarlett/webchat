bd55ae6c10d07fee3de8ad9d045a66bf4645ca8b
Skye <anastasia_sorokina_90@mail.ru>
2024-03-21 12:48:37 +0300

    sessions and messages models added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: 'Skye', password: 'password')
+User.create(username: 'User1', password: 'password')
+User.create(username: 'User2', password: 'password')
+User.create(username: 'User3', password: 'password')
+User.create(username: 'User4', password: 'password')
+
+
+Message.create(body: 'Hey', user_id: 3)
+Message.create(body: 'Hi', user_id: 2)
+Message.create(body: 'Hello', user_id: 1)
+Message.create(body: 'Buenos dias', user_id: 4)
+Message.create(body: 'Welcome to the Hahniverse', user_id: 5)
