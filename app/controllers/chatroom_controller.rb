e729d25533db429709c6e5a760420b6e7cdf151f
Skye <anastasia_sorokina_90@mail.ru>
2024-03-21 13:30:45 +0300

    actual message displaying added

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,2 +1,12 @@
 class ChatroomController < ApplicationController
+
+    def index
+        @messages = Message.all
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:body, :user_id)
+    end
 end
