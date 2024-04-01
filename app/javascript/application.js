a5e249ae0e9e95ed3400e074a1b7ee507aee59cd
Skye <anastasia_sorokina_90@mail.ru>
2024-04-01 16:02:53 +0300

    scroller problems

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,10 +4,22 @@ import "controllers"
 import "@hotwired/turbo-rails"
 import * as jquery from "jquery"
 import "semantic-ui"
+
+function scrollDown() {
+  var $messages = $('.message_container');
+  var $messageBlock = $messages.children('.message');
+
+  if ($messageBlock.length > 1) {
+    $messages.scrollTop($messages[0].scrollHeight - $messages[0].clientHeight);
+  }
+}
+
 $(document).on('turbo:load', function() {
     console.log('loaded turbo links')
     $('.ui.dropdown').dropdown()
         $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scrollDown();
 });
+import "./channels"
